e01935f23b3f611be04499c80b749d512763d087
mithun <49356216+MithunMi97@users.noreply.github.com>
2019-10-03 23:10:01 +0530

    day1routescreation

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 const express = require("express");
-const app = express();
+const mongoose = require('mongoose')
 require("dotenv").config();
+//import routes
+const userRoutes = require('./routes/user')
 
-app.get("/", (req, res) => {
-    res.send("hello from node");
-});
+//app
+const app = express();
+
+//db
+mongoose.connect(process.env.DATABASE, {
+    useNewUrlParse: true,
+    useCreateIndex: true
+}).then(() => console.log("DB Connected"));
+
+//routes middleware
+app.use("/api", userRoutes)
+
 const port = process.env.PORT || 8000;
 
 app.listen(port, () => {
