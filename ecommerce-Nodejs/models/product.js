cdcd594841ce2d295f548e245912bf1cbc3e8f94
mithun <49356216+MithunMi97@users.noreply.github.com>
2019-10-23 20:35:51 +0530

    Add view product and cart option

--- ecommerce-Nodejs/models/product.js
+++ ecommerce-Nodejs/models/product.js
@@ -7,7 +7,7 @@ const productSchema = new mongoose.Schema(
             type: String,
             trim: true,
             required: true,
-            maxlength: 50
+            maxlength: 32
         },
         description: {
             type: String,
