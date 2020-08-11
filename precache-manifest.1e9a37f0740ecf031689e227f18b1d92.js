257a788709cccf7190f3944d967a642488bf6ba7
mithun <49356216+MithunMi97@users.noreply.github.com>
2020-08-12 01:00:16 +0530

    Updates

---
+++ precache-manifest.1e9a37f0740ecf031689e227f18b1d92.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1c4f0ef77e31a04a9e7b4781c14b9ba",
+    "url": "/e-commerce-application_reactjs_and_nodejs/index.html"
+  },
+  {
+    "revision": "c31647361da34279a9d0",
+    "url": "/e-commerce-application_reactjs_and_nodejs/static/css/main.801ac66d.chunk.css"
+  },
+  {
+    "revision": "a7d3687b74df8a9706a6",
+    "url": "/e-commerce-application_reactjs_and_nodejs/static/js/2.45fac519.chunk.js"
+  },
+  {
+    "revision": "c31647361da34279a9d0",
+    "url": "/e-commerce-application_reactjs_and_nodejs/static/js/main.253ce527.chunk.js"
+  },
+  {
+    "revision": "eef20ac64772802e90ff",
+    "url": "/e-commerce-application_reactjs_and_nodejs/static/js/runtime~main.6b6feec4.js"
+  }
+]);
